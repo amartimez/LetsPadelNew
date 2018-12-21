dbc6ae87e661b8780702d71f68553a5d8fb7aa05
amartimez <amartimez@elpuig.xeill.net>
2018-12-21 12:45:52 +0100

    pushasso

--- app/src/main/java/com/example/padel_proyect/MainActivity.java
+++ app/src/main/java/com/example/padel_proyect/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+//albert bartolo
+
 public class MainActivity extends AppCompatActivity {
     Button btnInicioSesion;
     Button btnRegistro;
